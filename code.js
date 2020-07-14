5b961266d96af396911b72a8068dce19c06e5bf7
Yasir <Yasir.A.Kulane@gmail.com>
2020-07-14 13:26:40 -0700

    finished assignment

--- code.js
+++ code.js
@@ -13,17 +13,19 @@ let sentence = ["The", "dog", "jumped", "over", "the", "fence"];
 
 // Step One ------------------
 // Log the word "jumped" to the console by accessing it in the array.
-
+console.log(sentence[2]);
 // Your Code Here
 
 // Step Two ------------------
 // Log the last word in the array to the console.
 
 // Your Code Here
-
+console.log(sentence[5]);
 /*
 ---------------------------------------------------------------------------
 Exercise Two - Adding up numbers
+
+
 
 Create a statement which adds up every number of the array, assigning 
 the sum to a new variable.
@@ -32,7 +34,11 @@ Then log the sum to the console. (Hint: It should be 28)
 */
 
 let numbers = [3, 4, 5, 7, 9];
-
+function sum(acc, val) {
+  return acc + val;
+}
+let answer = numbers.reduce(sum);
+console.log(answer);
 // Your Code Here
 
 /*
@@ -44,12 +50,13 @@ Exercise Three - Creating an array
 // Step One ------------------
 // Create a new array and initialize with the names of five different colors.
 // The array should be assigned to a variable named "colors"
-
 // Your Code Here
+let colors = ["red", "blue", "green", "orange", "purple"];
 
 // Step Two ------------------
 // Log the length of the array to the console (it should print 5)
 
+console.log(colors.length);
 /*
 ---------------------------------------------------------------------------
 Exercise Four - Updating an array
@@ -63,14 +70,22 @@ let names = []; // Don't modify this line.
 // Then log your array to the console.
 
 // Your Code Here
+names[0] = "John";
+names[1] = "Jack";
+names[2] = "Michelle";
+names[3] = "Jordon";
+names[4] = "Mike";
 
+console.log(names);
 // Step Two ------------------
 // Using the index variable, assign a new name to the given index.
 // Then log the modified array to the console.
 let index = 2;
 
 // Your Code Here
+index = "hi";
 
+console.log(index);
 /*
 ---------------------------------------------------------------------------
 Exercise Five - Concatenating an array
@@ -86,3 +101,6 @@ let firstArray = ["This", "will", "make"];
 let secondArray = ["a", "combined", "array."];
 
 // Your Code Here.
+let combinedArr = firstArray.concat(secondArray);
+
+console.log(combinedArr);
